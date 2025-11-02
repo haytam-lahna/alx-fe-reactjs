64f9e8108403ed87328d37c340f26a2dc2ae1ccb
haytam-lahna <haytamleo10@gmail.com>
2025-11-02 21:19:12 +0100

    Add UserProfile component with props

---
+++ alx-react-app/Header.jsx
@@ -0,0 +1,18 @@
+function Header() {
+  return (
+    <header>
+      <h1>My Favorite Cities</h1>
+    </header>
+  );
+}
+
+cat > Header.jsx << 'EOF'
+function Header() {
+  return (
+    <header>
+      <h1>My Favorite Cities</h1>
+    </header>
+  );
+}
+
+export default Header;
