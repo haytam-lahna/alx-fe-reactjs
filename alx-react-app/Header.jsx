7cab4eb4e133e64e78f741eafce0ac12fb04c513
haytam-lahna <haytamleo10@gmail.com>
2025-11-02 21:44:05 +0100

    Add and modify WelcomeMessage component

--- alx-react-app/Header.jsx
+++ alx-react-app/Header.jsx
@@ -6,13 +6,4 @@ function Header() {
   );
 }
 
-cat > Header.jsx << 'EOF'
-function Header() {
-  return (
-    <header>
-      <h1>My Favorite Cities</h1>
-    </header>
-  );
-}
-
 export default Header;
