dd597659af2e9cabebad48d27971934a0eb3109e
haytam-lahna <haytamleo10@gmail.com>
2025-11-09 18:10:28 +0100

    Create Navbar.jsx

---
+++ my-company/src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <nav style={{
+      backgroundColor: '#333',
+      padding: '15px',
+      marginBottom: '20px'
+    }}>
+      <ul style={{
+        listStyle: 'none',
+        display: 'flex',
+        gap: '20px',
+        margin: 0,
+        padding: 0
+      }}>
+        <li>
+          <Link to="/" style={{ color: 'white', textDecoration: 'none' }}>Home</Link>
+        </li>
+        <li>
+          <Link to="/about" style={{ color: 'white', textDecoration: 'none' }}>About</Link>
+        </li>
+        <li>
+          <Link to="/services" style={{ color: 'white', textDecoration: 'none' }}>Services</Link>
+        </li>
+        <li>
+          <Link to="/contact" style={{ color: 'white', textDecoration: 'none' }}>Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
