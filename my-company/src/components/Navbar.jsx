b7071cfb662298f115baf094faf7e1907caaf370
haytam-lahna <haytamleo10@gmail.com>
2025-11-09 18:17:12 +0100

    Update Navbar.jsx

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -5,28 +5,14 @@ function Navbar() {
     <nav style={{
       backgroundColor: '#333',
       padding: '15px',
-      marginBottom: '20px'
+      marginBottom: '20px',
+      display: 'flex',
+      justifyContent: 'space-around'
     }}>
-      <ul style={{
-        listStyle: 'none',
-        display: 'flex',
-        gap: '20px',
-        margin: 0,
-        padding: 0
-      }}>
-        <li>
-          <Link to="/" style={{ color: 'white', textDecoration: 'none' }}>Home</Link>
-        </li>
-        <li>
-          <Link to="/about" style={{ color: 'white', textDecoration: 'none' }}>About</Link>
-        </li>
-        <li>
-          <Link to="/services" style={{ color: 'white', textDecoration: 'none' }}>Services</Link>
-        </li>
-        <li>
-          <Link to="/contact" style={{ color: 'white', textDecoration: 'none' }}>Contact</Link>
-        </li>
-      </ul>
+      <Link to="/" style={{ color: 'white', textDecoration: 'none' }}>Home</Link>
+      <Link to="/about" style={{ color: 'white', textDecoration: 'none' }}>About</Link>
+      <Link to="/services" style={{ color: 'white', textDecoration: 'none' }}>Services</Link>
+      <Link to="/contact" style={{ color: 'white', textDecoration: 'none' }}>Contact</Link>
     </nav>
   );
 }
