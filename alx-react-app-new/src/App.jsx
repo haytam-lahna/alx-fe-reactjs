b9679a5d20d387341c162e244f6a293ff5615504
haytam-lahna <haytamleo10@gmail.com>
2025-11-09 17:50:11 +0100

    Create App component with layout and user profile

---
+++ alx-react-app-new/src/App.jsx
@@ -0,0 +1,21 @@
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
+import UserProfile from './components/UserProfile';
+import WelcomeMessage from './components/WelcomeMessage';
+import Counter from './components/Counter';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <WelcomeMessage />
+      <Counter />
+      <MainContent />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
