b5faa9633756f3afff690229ab4a691f800fe2e4
haytam-lahna <haytamleo10@gmail.com>
2025-11-23 15:08:06 +0100

    Import fetchUserData in Search component for API handling

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { searchUsers } from '../services/githubService';
+import { searchUsers, fetchUserData } from '../services/githubService';
 
 const Search = () => {
   const [username, setUsername] = useState('');
