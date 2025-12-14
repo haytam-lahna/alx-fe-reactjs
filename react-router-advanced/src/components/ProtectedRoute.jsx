e66a5e8d9e78be5ee8f75cf550670a8e1a0b9af0
haytam-lahna <haytamleo10@gmail.com>
2025-12-14 11:22:56 +0100

    Fix ProtectedRoute to include useAuth hook

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,9 +1,17 @@
 import { Navigate } from "react-router-dom";
 
-function ProtectedRoute({ isAuthenticated, children }) {
+function useAuth() {
+  const isAuthenticated = true;
+  return { isAuthenticated };
+}
+
+function ProtectedRoute({ children }) {
+  const { isAuthenticated } = useAuth();
+
   if (!isAuthenticated) {
     return <Navigate to="/" replace />;
   }
+
   return children;
 }
 
